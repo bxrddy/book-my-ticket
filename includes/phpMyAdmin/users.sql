b176396f57cca1081af139b317c949baa9d4c430
Brijesh <brijesh.reddy15@siesgst.ac.in>
2017-10-26 00:32:19 +0530

    Updated [users.sql]

--- includes/phpMyAdmin/users.sql
+++ includes/phpMyAdmin/users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 25, 2017 at 04:33 PM
+-- Generation Time: Oct 25, 2017 at 08:59 PM
 -- Server version: 10.1.26-MariaDB
 -- PHP Version: 7.1.9
 
@@ -38,25 +38,26 @@ CREATE TABLE `users` (
   `address` varchar(255) DEFAULT NULL,
   `pincode` int(50) DEFAULT NULL,
   `password` varchar(255) NOT NULL,
-  `balance` int(50) NOT NULL DEFAULT '100'
+  `balance` int(50) NOT NULL DEFAULT '100',
+  `security_code` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `gender`, `city`, `state`, `address`, `pincode`, `password`, `balance`) VALUES
-(1, 'Brijesh', 'brijesh.reddy15@siesgst.ac.in', 'Male', 'Mumbai', 'Maharashtra', 'Chembur', 400089, 'brijesh', 870),
-(2, 'Ronaldo', 'cristiano.ronaldo07@gmail.com', 'Male', 'Madrid', 'Community of Madrid', 'Santiago Bernabeu', 28052, 'halamadrid', 80),
-(3, 'Messi', 'lionel.messi10@gmail.com', 'Male', 'Barcelona', 'Catalonia', 'Camp Nou', 8019, 'forcabarca', 100),
-(4, 'Mark', 'mark.zuck@facebook.com', 'Male', 'New York', 'U.S', 'White Plains', 10029, 'facebook', 300),
-(5, 'Marcelo', 'marcelo12@realmadrid.com', 'Male', 'Madrid', 'Community of Madrid', 'Santiago Bernabeu', 28059, 'halamadrid', 100),
-(6, 'Arpita', 'arpita.arpita15@siesgst.ac.in', 'Female', 'Navi mumbai', 'Maharashtra', 'Airolii', 400708, 'arpita', 1870),
-(7, 'Omkar', 'omkar.prabhu15@siesgst.ac.in', 'Male', 'Mumbai', 'maharashtra', 'Badlapur', 421503, 'omkar', 750),
-(8, 'Aditya', 'aditya.kulkarni15@siesgst.ac.in', 'Male', 'Navi Mumbai', 'Maharashtra', 'Koparkhairane ', 400709, 'aditya', 520),
-(9, 'Mohit', 'mohit.parulekar15@siesgst.ac.in', 'Male', 'Mumbai', 'Maharashtra', 'Tilak Nagar', 400089, 'mohit', 210),
-(10, 'Hailee', 'haileesteinfeld@rockbottom.com', 'Female', 'Greenville', 'South Carolina', 'Santa Monica', 29601, 'hailee', 880),
-(11, 'Kygo', 'kygo@thistown.com', 'Male', 'Greenwood', 'Colorado', 'Orchard Road', 80019, 'kygo', 120);
+INSERT INTO `users` (`id`, `name`, `email`, `gender`, `city`, `state`, `address`, `pincode`, `password`, `balance`, `security_code`) VALUES
+(1, 'Brijesh', 'brijesh.reddy15@siesgst.ac.in', 'Male', 'Mumbai', 'Maharashtra', 'Chembur', 400089, 'brijesh', 870, 222),
+(2, 'Ronaldo', 'cristiano.ronaldo07@gmail.com', 'Male', 'Madrid', 'Community of Madrid', 'Santiago Bernabeu', 28052, 'halamadrid', 80, 333),
+(3, 'Messi', 'lionel.messi10@gmail.com', 'Male', 'Barcelona', 'Catalonia', 'Camp Nou', 8019, 'forcabarca', 100, 444),
+(4, 'Mark', 'mark.zuck@facebook.com', 'Male', 'New York', 'U.S', 'White Plains', 10029, 'facebook', 300, 555),
+(5, 'Marcelo', 'marcelo12@realmadrid.com', 'Male', 'Madrid', 'Community of Madrid', 'Santiago Bernabeu', 28059, 'halamadrid', 100, 666),
+(6, 'Arpita', 'arpita.arpita15@siesgst.ac.in', 'Female', 'Navi mumbai', 'Maharashtra', 'Airolii', 400708, 'arpita', 1890, 777),
+(7, 'Omkar', 'omkar.prabhu15@siesgst.ac.in', 'Male', 'Mumbai', 'maharashtra', 'Badlapur', 421503, 'omkar', 750, 888),
+(8, 'Aditya', 'aditya.kulkarni15@siesgst.ac.in', 'Male', 'Navi Mumbai', 'Maharashtra', 'Koparkhairane ', 400709, 'aditya', 520, 999),
+(9, 'Mohit', 'mohit.parulekar15@siesgst.ac.in', 'Male', 'Mumbai', 'Maharashtra', 'Tilak Nagar', 400089, 'mohit', 210, 223),
+(10, 'Hailee', 'haileesteinfeld@rockbottom.com', 'Female', 'Greenville', 'South Carolina', 'Santa Monica', 29601, 'hailee', 940, 334),
+(11, 'Kygo', 'kygo@thistown.com', 'Male', 'Greenwood', 'Colorado', 'Orchard Road', 80019, 'kygo', 120, 445);
 
 --
 -- Indexes for dumped tables
